9107a5e836ce3693c3c0328be28a5e6a257da4a6
Xamman27 <xamman27@gmail.com>
2023-07-18 19:03:10 +0400

    in progress

--- homework/models.py
+++ homework/models.py
@@ -57,17 +57,25 @@ class Cart:
         # По-умолчанию корзина пустая
         self.products = {}
 
+    def print_cart(self):
+        if len(self.products)== 0:
+            print('Корзина пуста')
+        else:
+
+            for i_key, i_value in self.products.items():
+                print(f'{i_key.name} - {i_value} шт.')
+
     def add_product(self, product: Product, buy_count=1):
         """
         Метод добавления продукта в корзину.
         Если продукт уже есть в корзине, то увеличиваем количество
         """
         if product in self.products:
-            self.products[product] += 1
+            self.products[product] += buy_count
         else:
             self.products[product] = buy_count
-        print(f'добавлен{self.products[product]} в количестве {buy_count}')
-            #  raise NotImplementedError
+        print(f'добавлен {product.name} в количестве {buy_count} шт.')
+        #  raise NotImplementedError
 
     def remove_product(self, product: Product, remove_count=None):
         """
@@ -75,18 +83,18 @@ class Cart:
         Если remove_count не передан, то удаляется вся позиция
         Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
         """
-        if Product in product:
-            if remove_count is None or remove_count > product[Product]:
-                product.pop(Product)
+        if product in self.products:
+            if None == remove_count or remove_count > self.products[product]:
+                self.products.pop(product)
             else:
-                product[Product] -= remove_count
+                self.products[product] -= remove_count
         else:
             print("Product not in cart")
-        raise NotImplementedError
+            raise NotImplementedError
 
     def clear(self):
-        self.clear()
-        raise NotImplementedError
+        self.products.clear()
+        # raise NotImplementedError
 
     def get_total_price(self) -> float:
         total_price = 0
@@ -107,6 +115,15 @@ class Cart:
         """
         # raise NotImplementedError
 
-potato = Product('potato', 20, "good",2)
+
+potato = Product('potato', 20, "good", 2)
+apple = Product('apple', 22, "fruit", 5)
 my_cart = Cart()
 my_cart.add_product(potato, 2)
+my_cart.remove_product(potato,1)
+my_cart.print_cart()
+my_cart.clear()
+my_cart.print_cart()
+my_cart.add_product(apple, 2)
+my_cart.add_product(potato)
+my_cart.print_cart()

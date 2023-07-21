79585af9c74d8084fbb0730af05a2ffec522cca0
Xamman27 <xamman27@gmail.com>
2023-07-21 14:13:25 +0400

    testing class cart

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -3,13 +3,16 @@
 """
 import pytest
 
-from homework.models import Product
+from homework.models import Product, Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
 
+@pytest.fixture
+def cart():
+    return Cart()
 
 class TestProducts:
     """
@@ -35,7 +38,7 @@ class TestProducts:
         # TODO напишите проверки на метод buy,
         #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
         with pytest.raises(ValueError):
-            product.buy(1000)
+            product.buy(1001)
         pass
 
 
@@ -46,3 +49,12 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+    def test_add_new_product(self, cart, product):
+        cart.add_product(product, 10)
+        assert cart.products[product] == 10
+
+    def test_add_not_new_product(self, cart, product):
+        cart.add_product(product)
+        cart.add_product(product, 10)
+        assert len(cart.products) == 1
+        assert cart.products[product] == 11
